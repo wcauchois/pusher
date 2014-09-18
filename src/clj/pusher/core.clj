e16588aefc0aa43a77f443c8fa8895a9fc91a643
Bill Cauchois <wcauchois@gmail.com>
2014-09-17 21:55:37 -0400

    add annoying thread

--- src/clj/pusher/core.clj
+++ src/clj/pusher/core.clj
@@ -6,7 +6,8 @@
             [monger.util :refer [object-id]]
             [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.adapter.jetty :refer [run-jetty]]))
+            [ring.adapter.jetty :refer [run-jetty]])
+  (:import [java.util.concurrent Executor]))
 
 (def ^:dynamic *mongo-conn* nil)
 (def ^:dynamic *mongo-db* nil)
@@ -22,6 +23,22 @@
           (map (fn [u] (update-in u [:_id] #(.toString %))) all-users)]
     (json-resp {:users rendered-users})))
 
+(defn start-polling-thread []
+  (let [exit-atom (atom false)]
+    [(doto
+       (Thread.
+         (fn []
+           (println "Waking up")
+           (try (Thread/sleep 1000) (catch InterruptedException e nil))
+           (if (not @exit-atom) (recur))))
+       (.start))
+     exit-atom]))
+
+(defn stop-polling-thread [t]
+  (let [thread (nth t 0) exit-atom (nth t 1)]
+    (swap! exit-atom (fn [_] true))
+    (doto thread (.interrupt) (.join))))
+
 (defroutes app
   (GET "/" [] "Hello")
   (GET "/api/v1/listusers" [] list-users-handler)
@@ -31,5 +48,11 @@
   ; Connect to Mongo instance on localhost
   (alter-var-root (var *mongo-conn*) (fn [_] (mg/connect)))
   (alter-var-root (var *mongo-db*) (fn [_] (mg/get-db *mongo-conn* "pusher")))
-  (run-jetty app {:port 8080}))
+  (let [polling-thread (start-polling-thread)]
+    (.addShutdownHook
+      (Runtime/getRuntime)
+      (Thread. (fn []
+                 (println "Shutting down")
+                 (stop-polling-thread polling-thread))))
+    (run-jetty app {:port 8080})))
 
